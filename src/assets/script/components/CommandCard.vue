ec075767b7ebe2e453a6f46fd16553d754f61ef4
Veren <sunyatasattva@gmail.com>
2018-05-08 01:53:19 +0200

    Implements skills in Command Card and Cooldown spinner. Closes #37

--- src/assets/script/components/CommandCard.vue
+++ src/assets/script/components/CommandCard.vue
@@ -9,10 +9,21 @@
       :character="character"
       commandId="hunkerDown"
       />
+      
+    <command-button 
+      v-for="skill in character.attributes.skills"
+      v-if="skill.cooldown"
+      :character="character"
+      :commandId="skill.id"
+      :currentCooldown="skill.currentCooldown"
+      @command="startCooldown"
+    />
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
+
 import CommandButton from './CommandButton.vue';
   
 export default {
@@ -20,6 +31,14 @@ export default {
   props: ['character'],
   components: {
     CommandButton
+  },
+  methods: {
+    startCooldown(command) {
+      const skill = this.character.attributes.skills
+        .find( _ => _.id === command.id );
+      
+      Vue.set(skill, 'currentCooldown', skill.cooldown);
+    }
   }
 }
 </script>
@@ -27,19 +46,37 @@ export default {
 <style scoped lang="scss">
   .command-card {
     position: fixed;
-    bottom: 24px;
-    left: 50%;
+    bottom:   24px;
+    left:     50%;
     
     .command-button {
+      cursor:  pointer;
       display: inline-block;
-      margin: 0 2px;
-      width: 50px;
-      cursor: pointer;
-      height: 50px;
-      opacity: 0.7;
+      margin:  0 2px;
+      opacity: 0.8;
+      width:   50px;
+      height:  50px;
+      
+      transition: all 0.1s ease-in-out;
+      
+      &:active {
+        transform: scale(1.1);
+      }
       
       &:hover {
         opacity: 1;
+      }
+      
+      &.is-cooldown {
+        cursor: progress;
+        
+        &:active {
+          transform: scale(1);
+        }
+        
+        &:hover {
+          opacity: 0.7;
+        }
       }
     }
   }
